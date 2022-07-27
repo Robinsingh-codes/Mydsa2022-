1c404bc1877aa8bfa7956a52da7bcd3e70740439
Robin Singh <85799231+Robinsingh-codes@users.noreply.github.com>
2022-07-27 17:44:56 +0530

    Update ArrayUnion.java

--- ArrayUnion.java
+++ ArrayUnion.java
@@ -12,3 +12,25 @@ class Solution{
       return hs.size();
     }
 }
+***********************************
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        HashSet<Integer> hs = new HashSet<Integer>();
+        
+        for(int i=0;i<n;i++)hs.add(arr1[i]);
+        for(int j=0;j<m;j++)hs.add(arr2[j]);
+        
+        ArrayList<Integer> al=new ArrayList<>();
+        for(int ob:hs){
+            al.add(ob);
+        }
+        
+        
+        Collections.sort(al);
+        return al;
+    }
+}
+
